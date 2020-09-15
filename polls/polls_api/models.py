2a047356df0b64ca608514e4781c9790c0581928
Sergey Bryukhovskikh <sergey.bryukhovskikh@gmail.com>
2020-09-15 16:38:47 +0500

    Добавлена модель

---
+++ polls/polls_api/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+
+
+class poll(models.Model):
+    """Опрос"""
+    name = models.CharField('Название', max_length=50)
+    start_date = models.DateField('Дата начала')
+    end_date = models.DateField('Дата окончания')
+    description = models.TextField('Описание')
+
+
+class question(models.Model):
+    """Вопрос"""
+    question = models.ForeignKey(poll, on_delete=models.CASCADE)
+    question_types = [
+        (0, 'Тектовый'),
+        (1, 'Один вариант'),
+        (2, 'Несколько вариантов')
+    ]
+    text = models.TextField('Текст вопроса')
+    type = models.IntegerField(
+        'Тип вопроса',
+        choices=question_types,
+        default=0
+    )
+
+
+class choice(models.Model):
+    question = models.ForeignKey(question, on_delete=models.CASCADE)
+    name = models.CharField('Название', max_length=50)
+
+
+class answer(models.Model):
+    question = models.ForeignKey(question, on_delete=models.CASCADE)
+    choice = models.ForeignKey(choice, on_delete=models.CASCADE, null=True)
+    user_id = models.IntegerField('ID пользователя')
+    text = models.TextField('Текст ответа', null=True)
